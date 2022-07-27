f902d18ee85e791ac67992fc121ddcf36256b20a
Adam <AdamJankowski5@gmail.com>
2022-07-27 14:12:54 +0200

    Files added, consts ready, background ready, header ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Logo from "./assets/logo.svg";
+import Triangle from "./assets/bg-triangle.svg";
+import Paper from "./assets/icon-paper.svg";
+import Rock from "./assets/icon-rock.svg";
+import Scissors from "./assets/icon-scissors.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="logo-score-container">
+          <img className="logo" src={Logo} alt="logo" />
+          <div className="score-container">
+            <span className="score-text">Score</span>
+            <span className="score-number">12</span>
+          </div>
+        </div>
+        <div className="buttons-container">
+          <div className="upper-row"></div>
+          <div className="lower-row"></div>
+          <div className="triangle"></div>
+        </div>
+      </div>
     </div>
   );
 }
